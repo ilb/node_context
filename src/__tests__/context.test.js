70fae1c519a9dd9522148b291460d23d4c7aaf20
slavb <slavb@ilb.ru>
2019-11-18 07:07:35 +0000

    add jest

--- src/__tests__/context.test.js
+++ src/__tests__/context.test.js
@@ -6,5 +6,8 @@ const path = require('path');
 
 const contextPath = path.resolve('src/__tests__/context.xml');
 const contextxml = fs.readFileSync(contextPath, 'utf8');
-var result = context.config.parseContextXml(contextxml);
 
+const expected = {"ru.bystrobank.apps.workflow.certfile": "/certs/application.pem", "ru.bystrobank.apps.workflow.cert_PASSWORD": "cert_pass_here"};
+test('parses context.xml', () => {
+    expect(context.parseContextXml(contextxml)).toStrictEqual(expected);
+});
--- src/context.js
+++ src/context.js
@@ -1,20 +1,63 @@
+// Parses context.xml src into an Object
+function parseContextXml(src) {
+    const DOMParser = require('xmldom').DOMParser;
+    const xpath = require('xpath');
+    const obj = {};
+    const doc = new DOMParser().parseFromString(src.toString());
+    xpath.select('//Environment', doc).forEach(function (node, idx) {
+        const key = node.getAttribute('name');
+        let val = node.getAttribute('value');
+        obj[key] = val;
+    });
+    return obj;
+}
+
+// Parses web.xml src into an Object
+function parseWebXml(src) {
+    const DOMParser = require('xmldom').DOMParser;
+    const xpath = require('xpath');
+
+    const obj = {};
+    const doc = new DOMParser().parseFromString(src.toString());
+    var select = xpath.useNamespaces({"j2ee": "http://java.sun.com/xml/ns/j2ee"});
+
+    select('//j2ee:env-entry', doc).forEach(function (node, idx) {
+        const key = select('j2ee:env-entry-name', node)[0].firstChild.data;
+        const valnodelist = select('j2ee:env-entry-value', node);
+        const val = valnodelist.length > 0 ? valnodelist[0].firstChild.data : null;
+        obj[key] = val;
+    });
+    return obj;
+}
+
+function putEnv(webobj, contextobj, debug) {
+    Object.keys(webobj).forEach(function (key) {
+        const value = contextobj[key] ? contextobj[key] : webobj[key];
+        process.env[key] = value;
+        if (debug) {
+            console.log("putEnv: " + key + " = " + value);
+        }
+
+    });
+
+}
+
 function config(opts) {
     opts = opts || {};
     if (!process.browser) {
         const fs = require('fs');
         const path = require('path');
-        const DOMParser = require('xmldom').DOMParser;
-        const xpath = require('xpath');
 
         const contextPath = path.resolve(path.join(process.env.HOME, '.config/context.xml'));
 
         let contextobj = {};
         if (fs.existsSync(contextPath)) {
             const contextxml = fs.readFileSync(contextPath, 'utf8');
-            contextobj = this.parseContextXml(contextxml);
+            contextobj = parseContextXml(contextxml);
             if (opts.debug) {
                 console.log("contextobj: " + JSON.stringify(contextobj));
             }
+
         }
 
         let webobj = {};
@@ -25,53 +68,20 @@ function config(opts) {
 
         if (fs.existsSync(webxmlPath)) {
             const webxml = fs.readFileSync(webxmlPath, 'utf8');
-            webobj = this.parseWebXml(webxml);
+            webobj = parseWebXml(webxml);
             if (opts.debug) {
                 console.log("webobj: " + JSON.stringify(webobj));
             }
+
         }
 
-        putEnv(webobj, contextobj);
+        putEnv(webobj, contextobj, opts.debug);
 
-        // Parses context.xml src into an Object
-        this.parseContextXml = function (src) {
-            const obj = {};
-            const doc = new DOMParser().parseFromString(src.toString());
-            xpath.select('//Environment', doc).forEach(function (node, idx) {
-                const key = node.getAttribute('name');
-                let val = node.getAttribute('value');
-                obj[key] = val;
-            });
-            return obj;
-        }
-
-        // Parses web.xml src into an Object
-        this.parseWebXml = function (src) {
-            const obj = {};
-            const doc = new DOMParser().parseFromString(src.toString());
-            var select = xpath.useNamespaces({ "j2ee": "http://java.sun.com/xml/ns/j2ee" });
-
-            select('//j2ee:env-entry', doc).forEach(function (node, idx) {
-                const key = select('j2ee:env-entry-name', node)[0].firstChild.data;
-                const valnodelist = select('j2ee:env-entry-value', node);
-                const val = valnodelist.length > 0 ? valnodelist[0].firstChild.data : null;
-                obj[key] = val;
-            });
-            return obj;
-        }
-
-        this.putEnv = function (webobj, contextobj) {
-            Object.keys(webobj).forEach(function (key) {
-                const value = contextobj[key] ? contextobj[key] : webobj[key];
-                process.env[key] = value;
-                if (opts.debug) {
-                    console.log("putEnv: " + key + " = " + value);
-                }
-            });
-        }
     }
 }
 
 module.exports = {
-    config
+    config,
+    parseContextXml,
+    parseWebXml
 };
