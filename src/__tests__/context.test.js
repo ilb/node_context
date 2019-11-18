b624da2234b021071e01ae24b14be77a73260848
slavb <slavb@ilb.ru>
2019-11-18 06:54:02 +0000

    add jest

---
+++ src/__tests__/context.test.js
@@ -0,0 +1,10 @@
+var context = require('../context');
+
+//import context from '../context';
+const fs = require('fs');
+const path = require('path');
+
+const contextPath = path.resolve('__tests__/context.xml');
+const contextxml = fs.readFileSync(contextPath, 'utf8');
+var result = context.config.parseContextXml(contextxml);
+
