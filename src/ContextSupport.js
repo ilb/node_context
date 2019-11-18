f26d4de8703c50c3c5de50ecb6e7f4ded1be6d0a
slavb <slavb@ilb.ru>
2019-11-18 10:21:01 +0000

    docs

---
+++ src/ContextSupport.js
@@ -0,0 +1,15 @@
+class ContextSupport {
+    constructor(webXmlUrl, contextXmlUrl) {
+
+    }
+
+    /**
+     * Function populates process.env context with values read from web.xml and context.xml
+     * context.xml values have higher priority
+     * @returns {undefined}
+     */
+    buildContext() {
+
+    }
+
+}
