8a7b4112daae1e4a285468ec0f59d3c182ec3954
Vjacheslav V. Borisov <slavb18@gmail.com>
2019-12-12 11:42:50 +0000

    introduce OpenLDAPConfig
    git-svn-id: https://svn.ilb.ru/repos/node_context/trunk@22 bce5d2fa-9b4c-4a37-a3bc-91235413efd1

--- src/LDAP/__tests__/OpenLDAPConfig.test.js
+++ src/LDAP/__tests__/OpenLDAPConfig.test.js
@@ -1,17 +1,17 @@
-import LDAPConfig from '../LDAPConfig';
+import OpenLDAPConfig from '../OpenLDAPConfig';
 import * as path from 'path';
 
 const fs = require('fs');
 const ldapConfPath = path.resolve('src/LDAP/__tests__/ldap.conf');
 
-const ldapConfig = new LDAPConfig(ldapConfPath);
+const ldapConfig = new OpenLDAPConfig(ldapConfPath);
 
 const expectedConfig = {BASE: 'dc=example,dc=com',
     TLS_CACERT: "/etc/ssl/certs/ourCAbundle.crt",
     URI: 'ldapi:/// ldaps://devel.net.ilb.ru ldaps://ldap.net.ilb.ru ldaps://ldap2.net.ilb.ru'};
 
 test('parseConfig', () => {
-    expect(LDAPConfig.parseConfig(ldapConfPath)).toStrictEqual(expectedConfig);
+    expect(OpenLDAPConfig.parseConfig(ldapConfPath)).toStrictEqual(expectedConfig);
 });
 
 
