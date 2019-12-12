c06cd6479291273e359c65bff17552033fd736c6
slavb <slavb@ilb.ru>
2019-12-12 11:42:50 +0000

    introduce OpenLDAPConfig

--- src/LDAP/LDAPConfig.js
+++ src/LDAP/LDAPConfig.js
@@ -1,40 +1,9 @@
 export default class LDAPConfig {
 
-    constructor(configPath) {
-        this.configPath = configPath || '/etc/openldap/ldap.conf';
+    constructor() {
         this.uri = [];
         this.base = null;
         this.caCert = null;
-        this.ldapSchemasRegexp = /^ldaps?:\/\//;
-
-        this.loadValuesFromConfig();
-    }
-
-    loadValuesFromConfig() {
-        const config = LDAPConfig.parseConfig(this.configPath);
-        if (config.URI) {
-            this.ldapUri = config.URI.split(/\s+/).filter(l => l.match(this.ldapSchemasRegexp));
-        }
-        this.base = config.BASE || null;
-        this.caCert = config.TLS_CACERT || null;
-    }
-
-    static parseConfig(configPath) {
-        const fs = require('fs');
-        const config = fs.readFileSync(configPath, 'utf8');
-        const alllines = config.split(/\r?\n/);
-        const lines = alllines
-                .map(l => l.replace(/#.*$/, '').trim()) // remove comments and trim
-                .filter(l => l.length > 0); // skip empty lines
-        //console.log(lines);
-        const map = lines
-                .map(l => l.split(/\s(.*)/)) // split by first whitespace
-                .reduce(function (map, obj) {
-                    map[obj[0]] = obj[1];
-                    return map;
-                }, {});
-        //console.log(map);
-        return map;
     }
 
     getLdapUri() {
