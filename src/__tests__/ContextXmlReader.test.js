d4a6819e70fdd7f3ca456235054119792af2e6ef
Vjacheslav V. Borisov <slavb18@gmail.com>
2019-12-12 04:22:10 +0000

    implement ContextXmlReader and WebXmlReader
    git-svn-id: https://svn.ilb.ru/repos/node_context/trunk@13 bce5d2fa-9b4c-4a37-a3bc-91235413efd1

--- src/__tests__/ContextXmlReader.test.js
+++ src/__tests__/ContextXmlReader.test.js
@@ -1,13 +1,14 @@
 import ContextXmlReader from '../ContextXmlReader';
 import * as path from 'path';
 
+const fs = require('fs');
 const contextPath = path.resolve('src/__tests__/context.xml');
-const wxr = new ContextXmlReader(contextPath);
+const cxr = new ContextXmlReader(fs.readFileSync(contextPath));
 
 const expected = {
     "ru.bystrobank.apps.workflow.certfile": "/certs/application.pem",
     "ru.bystrobank.apps.workflow.cert_PASSWORD": "cert_pass_here"
 };
 test('parses context.xml', () => {
-    expect(wxr.getValues()).toStrictEqual(expected);
+    expect(cxr.getValues()).toStrictEqual(expected);
 });
